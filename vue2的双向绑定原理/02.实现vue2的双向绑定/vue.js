ba8088fb2fe90b5661a879a29e9941e55e15d3fa
Marguerite <1546716081@qq.com>
2022-03-06 23:49:55 +0800

    完成data到页面的单向数据绑定

--- vue2的双向绑定原理/02.实现vue2的双向绑定/vue.js
+++ vue2的双向绑定原理/02.实现vue2的双向绑定/vue.js
@@ -25,6 +25,7 @@ class Vue{
 
 function Observer(obj){
     if(!obj || typeof obj != 'object')return
+    const dep = new Dep();
     Object.keys(obj).forEach((key)=>{
         let value = obj[key];
         Observer(value)
@@ -32,12 +33,15 @@ function Observer(obj){
             configurable: true, 
             enumerable: true,
             get(){
+                Dep.target && dep.addSub(Dep.target);
                 return value
             },
             set(newValue){
                 value = newValue
                 // 为新赋值的对象，也添加上getter和setter方法
                 Observer(value)
+                // 通知每一个订阅者更新值
+                dep.notify()
             }
         })
     })
@@ -66,14 +70,53 @@ function Compile(el, vm){
             const result = regMustache.exec(text)
             console.log(result);
             if(result){
-                node.textContent = result[1].split('.').reduce((obj, key)=>{
-                    return obj[key]
-                }, vm)   
+                const value = result[1].split('.').reduce((obj, key)=>{return obj[key]}, vm)   
+                node.textContent = text.replace(regMustache, value)
+                // node节点已经知道如何更新自己，所用在这里创建Wacter类的实例
+                new Watcher(vm, result[1], (newValue)=>{
+                    node.textContent = text.replace(regMustache, newValue)
+                })
             }
             return
         }
 
         // 不是文本节点，可能是一个dom元素，此时还有继续进行递归
         node.childNodes.forEach((item)=>replace(item))
+    }
+}
+
+
+class Dep{
+    constructor(){
+        this.subs = []
+    }
+    addSub(watcher){
+        this.subs.push(watcher)
+    }
+    notify(){
+        this.subs.forEach(watcher=>watcher.update())
+    }
+}
+
+class Watcher{
+    constructor(vm, key, cb){
+        this.vm = vm
+        this.key = key
+        this.cb = cb
+        /**
+         * 以下三行代码的解释
+         * 发布者即dep应该是只能有一个，所有的订阅者都在应该在同一个dep中订阅
+         * 当创建watcher时，会执行constructor方法，此时我们将该watcher即this存入Dep类当中。
+         * 然后我们进行一次取值操作，又因为我们给所用的属性都添加上了getter属性，所以会触发该属性的get方法
+         * 在Observer方法中，我们创建了一个dep对象。当执行get方法中，我们将该watcher添加到dep的subs中。
+         * 添加成功后，target就没有必要在指向watcher了，所以置空
+         */
+        Dep.target = this
+        key.split('.').reduce((obj, key)=>{return obj[key]}, vm)   
+        Dep.target = null
+    }
+    update(){
+        const value = this.key.split('.').reduce((obj, key)=>{return obj[key]}, vm)   
+        this.cb(value);
     }
 }
